7ef7bec38e173dbcf4a6a3fb167ecc7f6f2219ad
celinegraversen <celine.graversen@gmail.com>
2016-05-29 21:34:05 +0200

    Piccies

---
+++ js-image-slider.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Céline G TE14 on 2016-05-29.
+ */
+
+var sliderOptions =
+{
+    sliderId: "slider",
+    effect: "17,13,1",
+    effectRandom: true,
+    pauseTime: 2800,
+    transitionTime: 1200,
+    slices: 14,
+    boxes: 8,
+    hoverPause: 1,
+    autoAdvance: true,
+    captionOpacity: 0.4,
+    captionEffect: "fade",
+    thumbnailsWrapperId: "thumbs",
+    m: false,
+    license: "mylicense"
+};
